574f5e9d9d9f739628f08bd4824ecc1452ff1017
Elcoosp <elcoosp@gmail.com>
2018-05-05 22:21:45 +0200

    add: util to format the routes received as input into an object with a constant and path variation

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 const shellJs = require('shelljs')
+const { constantCase } = require('change-case')
 
 const compose = (...fns) => ({
   against: initAcc => fns.reduce((acc, f) => f(initAcc), initAcc)
@@ -9,15 +10,28 @@ const returnArg = sideEffectsFunc => arg => {
   return arg
 }
 
-const shell = command => ({
-  msg: msg => {
-    console.log(msg)
+const shellExec = command => ({
+  msg: (msg = false) => {
+    msg && console.log(msg)
     shellJs.exec(command, { silent: true })
   }
 })
 
+const toFormattedRoutes = routes =>
+  routes
+    .split(' ')
+    .filter(x => x.trim() !== '')
+    .reduce((acc, route) => {
+      acc[route] = {
+        constantFormat: constantCase(route),
+        pathFormat: '/' + route.toLowerCase()
+      }
+      return acc
+    }, {})
+
 module.exports = {
   compose,
   returnArg,
-  shell
+  toFormattedRoutes,
+  shellExec
 }
