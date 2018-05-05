b8663176f6010d8acd30eae8bb01f3c3b6c8fcb5
Elcoosp <elcoosp@gmail.com>
2018-05-05 22:21:55 +0200

    add: routes input

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const program = require('commander')
 const inquirer = require('inquirer')
-const { compose, shell } = require('./utils')
+const { compose, shell, upperUnderscoreSeparated, toFormattedRoutes } = require('./utils')
 const actions = require('./actions')
 
 const askQuestions = () =>
@@ -10,7 +10,19 @@ const askQuestions = () =>
       {
         type: 'input',
         name: 'projectName',
-        message: 'What is your project name ?'
+        message: 'What is your project name ? 🚀'
+      },
+      {
+        type: 'input',
+        name: 'publicRoutes',
+        filter: toFormattedRoutes,
+        message: 'Which public (one level deep) routes do you want to add (camelCase) ? 👀'
+      },
+      {
+        type: 'input',
+        name: 'privateRoutes',
+        filter: toFormattedRoutes,
+        message: 'Which private (one level deep) routes do you want to add (camelCase) ? 🚫'
       }
     ])
     .then(compose(...actions).against)
